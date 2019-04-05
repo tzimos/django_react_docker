3c27546358d5f28d6ed2f5e4f950ebc5492006b3
panos <panos@panosdebian>
2019-04-05 01:01:50 +0100

    Jenkins ruuuuuun

--- getip.sh
+++ getip.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-export API_HOST="http://$(ip route get 8.8.8.8 | awk 'NR==1 {print $NF}'):8000/backend"
+echo API_HOST="http://$(ip route get 8.8.8.8 | awk 'NR==1 {print $NF}'):8000/backend"
