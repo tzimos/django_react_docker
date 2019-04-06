83c1116fbe05a62d1c7dbe3c8c6c22932a10a665
panos <tzimoss@gmail.com>
2019-04-06 11:46:25 +0100

    backend settings updated to accept requests proxied by the domain name

--- backend/backend/settings/docker.py
+++ backend/backend/settings/docker.py
@@ -7,41 +7,41 @@
 
 from backend.settings.base import *
 
-ALLOWED_HOSTS += ['localhost', 'backend']
+domain_name = os.environ.get('DOMAIN_NAME', 'localhost')
+
+ALLOWED_HOSTS += [domain_name, 'backend']
 DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.postgresql_psycopg2',
-		'NAME': 'postgres',
-		'USER': 'postgres',
-		'PASSWORD': '{uS7M:Wg=(c@UuEg',
-		'HOST': 'django_react_db',
-	}
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '{uS7M:Wg=(c@UuEg',
+        'HOST': 'django_react_db',
+    }
 }
 
 DEBUG = bool(int(os.environ['DEBUG']))
 
 if DEBUG:
-	INSTALLED_APPS += [
-		'django_extensions',
-	]
+    INSTALLED_APPS += [
+        'django_extensions',
+    ]
 
-	if DEBUG_TOOLBAR:
-		INSTALLED_APPS += [
-			'debug_toolbar',
-		]
+    if DEBUG_TOOLBAR:
+        INSTALLED_APPS += [
+            'debug_toolbar',
+        ]
 
-		MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
-		INTERNAL_IPS = ['127.0.0.1', ]
+        MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
+        INTERNAL_IPS = ['127.0.0.1', ]
 
 if not DEBUG:
-	REST_FRAMEWORK = {
-		'DEFAULT_RENDERER_CLASSES': (
-			'rest_framework.renderers.JSONRenderer',
-		)
-	}
-
+    REST_FRAMEWORK = {
+        'DEFAULT_RENDERER_CLASSES': (
+            'rest_framework.renderers.JSONRenderer',
+        )
+    }
 CORS_ORIGIN_WHITELIST = (
-	'localhost:8080',
-	'localhost:8000',
-	'http://localhost:8000'
+    domain_name + ':8080',
+    domain_name + ':8000',
 )
